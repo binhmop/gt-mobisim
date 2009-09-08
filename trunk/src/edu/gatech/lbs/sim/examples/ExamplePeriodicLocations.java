60f7da77d35f72f562379ded51a66ca6dbac2674
pestip@gmail.com <pestip@gmail.com@aa7150e2-91d5-11de-a5e2-01e6cdb70936>
2009-09-08 05:56:52 +0000

    add header

--- trunk/src/edu/gatech/lbs/sim/examples/ExamplePeriodicLocations.java
+++ trunk/src/edu/gatech/lbs/sim/examples/ExamplePeriodicLocations.java
@@ -1,3 +1,7 @@
+// Copyright (c) 2009, Georgia Tech Research Corporation
+// Authors:
+//   Peter Pesti (pesti@gatech.edu)
+//
 package edu.gatech.lbs.sim.examples;
 
 import edu.gatech.lbs.sim.Simulation;
