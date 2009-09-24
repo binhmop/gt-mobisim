f97611344f753cedc6562c25eaf3dc37feed4ece
pestip <pestip@gmail.com>
2009-09-24 05:21:51 +0000

    bugfix: thirdparty jars on classpath was not working

--- configs/runjarex2.bat
+++ configs/runjarex2.bat
@@ -1,1 +1,1 @@
-java -Xmx500M -cp "../thirdparty/*.jar;../gtmobisim.jar" edu.gatech.lbs.sim.examples.ExampleAgentsOnSegment jnlp-demo.xml "150 sec" 4618
+java -Xmx500M -cp "../thirdparty/*;../gtmobisim.jar" edu.gatech.lbs.sim.examples.ExampleAgentsOnSegment jnlp-demo.xml "150 sec" 4618
