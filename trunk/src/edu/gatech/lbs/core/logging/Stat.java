fb2d2269f1c86b7ab1ea41424bda42f30d359fd2
pestip@gmail.com <pestip@gmail.com@aa7150e2-91d5-11de-a5e2-01e6cdb70936>
2013-08-21 04:39:28 +0000

    remove rounding method

--- trunk/src/edu/gatech/lbs/core/logging/Stat.java
+++ trunk/src/edu/gatech/lbs/core/logging/Stat.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2009, Georgia Tech Research Corporation
+// Copyright (c) 2012, Georgia Tech Research Corporation
 // Authors:
 //   Peter Pesti (pesti@gatech.edu)
 //
@@ -75,10 +75,5 @@ public class Stat {
     }
 
     return count;
-  }
-
-  public static double round(double v, int fractions) {
-    double tens = Math.pow(10, fractions);
-    return Math.round(v * tens) / tens;
   }
 }
