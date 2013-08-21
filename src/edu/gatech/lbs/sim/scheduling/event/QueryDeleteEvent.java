6eca060ae3425175dbaaa7484eb26f6b7dfd4714
pestip <pestip@gmail.com>
2013-08-21 05:19:03 +0000

    add .getSimAgentId() method

--- src/edu/gatech/lbs/sim/scheduling/event/QueryDeleteEvent.java
+++ src/edu/gatech/lbs/sim/scheduling/event/QueryDeleteEvent.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2009, Georgia Tech Research Corporation
+// Copyright (c) 2012, Georgia Tech Research Corporation
 // Authors:
 //   Peter Pesti (pesti@gatech.edu)
 //
@@ -38,6 +38,10 @@ public class QueryDeleteEvent extends SimEvent {
 
   public void execute() {
     sim.simulateRemoveQuery(simKey);
+  }
+
+  public int getSimAgentId() {
+    return simKey.uid;
   }
 
   public byte getTypeCode() {
